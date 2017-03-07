031ae7ae58cd76594fb1b032780dd65bb333faee
OrdynarySkalen <voron-37@mail.ru>
2017-03-08 02:14:58 +0700

    Создал файл заготовку под будущие тесты и коллекцию объектов User (пункт 2.ii)

---
+++ test1.bat
@@ -0,0 +1,4 @@
+java Application.java 123 123 12
+echo %ERRORLEVEL%
+java Application.java
+echo %ERRORLEVEL%
--- Задание №2 - Приложение по плану/src/Application.java
+++ Задание №2 - Приложение по плану/src/Application.java
@@ -1,17 +1,15 @@
+import java.util.ArrayList;
+
 /**
  * Created by Artem 2 on 06.03.2017.
  */
 public class Application {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.print(args[i] + "\n");
-
-            if (args.length == 2) {
-                System.exit(0);
-            } else {
-                System.exit(1);
-            }
-
+        User fPerson = new User("grom", "123qwe");
+        User nPerson = new User("groza", "ewq321");
+        ArrayList<User> Users = new ArrayList();
+        Users.add(fPerson);
+        Users.add(nPerson);
         }
     }
-}
+
