e2ba934893f595df2230cf88b5178cf2fe82a184
OrdynarySkalen <voron-37@mail.ru>
2017-03-10 00:24:35 +0700

    Создал класс Accounting

---
+++ Задание №2 - Приложение по плану/src/Accounting.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+
+/**
+ * Created by Artem 2 on 10.03.2017.
+ */
+public class Accounting {
+    private LocalDate dateStart;
+    private LocalDate dateFinish;
+    private int volume;
+    private Resource resource;
+    private User user;
+
+    public Accounting(LocalDate dateS, LocalDate dateF, int vol, Resource res, User user) {
+        this.setDateStart(dateS);
+        this.setDateFinish(dateF);
+        this.setVolume(vol);
+        this.setResource(res);
+        this.setUser(user);
+    }
+
+    public LocalDate getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(LocalDate dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public LocalDate getDateFinish() {
+        return dateFinish;
+    }
+
+    public void setDateFinish(LocalDate dateFinish) {
+        this.dateFinish = dateFinish;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
