200a92634a9118b5f13011a29c5db6af08d0682c
OrdynarySkalen <voron-37@mail.ru>
2017-03-09 14:02:20 +0700

    Корректируем работу программы под новый валидатор

--- Задание №2 - Приложение по плану/src/Application.java
+++ Задание №2 - Приложение по плану/src/Application.java
@@ -25,55 +25,52 @@ public class Application {
         Resourse Res2 = new Resourse("A.B.C", Id_users2, Roles.READ);
         Resourse[] massRes = {Res1, Res2};
 
-        validator.GetUserInput(args, userInput);
+        validator.SuperGetUserInput(userInput, args);
 
         User reqUser = new User();
         Resourse reqRes = new Resourse();
         int error = 0;
 
-        if (userInput.isAuthentification() == false) {
-            System.exit(-1); //вызов справки
-        } else {
-            reqUser = serv.FindUserByLogin(userInput.getLogin(), Users);//Найти юзера по логину
-            if (reqUser.getLogin() == null) {
-                System.exit(1);
+        reqUser = serv.FindUserByLogin(userInput.getLogin(), Users);//Найти юзера по логину
+        if (reqUser.getLogin() == null) {
+            System.exit(1);
+        }
+        if (serv.CheckPasswordByUser(reqUser, userInput.getPass()) == false) ; //Проверить пароль
+        {
+            System.exit(2);
+        }
+        System.out.print("Authentification: success");
+        //validator.GetResurseInput(args, userInput); // тут вылезет ошибка 3, если она есть
+        if (userInput.isAuthorisation() & error == 0) {
+            //исправить метод класса validator так, что бы было возможно получить
+            //ошибки 3,4 в этом фрагменте кода
+            error = 4;
+            //reqRes = serv.FindResourse(userInput.getRes(), userInput.getRole(), massRes); //найти ресурс по пути и роли
+            for (int i = 0; i < reqRes.getUsers_ID().length; i++) {
+                if (reqUser.getID() == reqRes.getUsers_ID()[i]) //проверка доступа
+                {
+                    error = 0;
+                    break;
+                }
             }
-            if (serv.CheckPasswordByUser(reqUser, userInput.getPass()) == false) ; //Проверить пароль
-            {
-                System.exit(2);
+            if (error == 4) {
+                System.exit(4);
             }
-            System.out.print("Authentification: success");
-            validator.GetResurseInput(args, userInput); // тут вылезет ошибка 3, если она есть
-            if (userInput.isAuthorisation() & error == 0) {
-                //исправить метод класса validator так, что бы было возможно получить
-                //ошибки 3,4 в этом фрагменте кода
-                error = 4;
-                reqRes = serv.FindResourse(userInput.getRes(), userInput.getRole(), massRes); //найти ресурс по пути и роли
-                for (int i = 0; i < reqRes.getUsers_ID().length; i++) {
-                    if (reqUser.getID() == reqRes.getUsers_ID()[i]) //проверка доступа
-                    {
-                        error = 0;
-                        break;
-                    }
-                }
-                if (error == 4) {
-                    System.exit(4);
-                }
-                System.out.print("Resourse " + reqRes.getPath() + " - ok");//разрешить доступ к ресурсу
-                //разрешить доступ к дочерним ресурсам с той же ролью
-                System.out.print("Authorisation: success");
-                validator.GetSessionInput(args, userInput);
-                if (userInput.isAccounting() & error == 0) {
-                    //isDataValid(ds,de);
-                    //isVolValid(vol)
-                    //tryGetDate()
-                    //tryGetVol()
-                    //добавить запись о посещении ресурса
-                    System.exit(0);
-                }
+            System.out.print("Resourse " + reqRes.getPath() + " - ok");//разрешить доступ к ресурсу
+            //разрешить доступ к дочерним ресурсам с той же ролью
+            System.out.print("Authorisation: success");
+            //validator.GetSessionInput(args, userInput);
+            if (userInput.isAccounting() & error == 0) {
+                //isDataValid(ds,de);
+                //isVolValid(vol)
+                //tryGetDate()
+                //tryGetVol()
+                //добавить запись о посещении ресурса
+                System.exit(0);
             }
         }
     }
 }
+
 
 
