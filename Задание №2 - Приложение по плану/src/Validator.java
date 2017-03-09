2e463e8ab4e4073a5187ef5a5a4cfe766ffaffe6
OrdynarySkalen <voron-37@mail.ru>
2017-03-09 13:59:19 +0700

    Корректирую валидатор
    по какой то причине help не выводится, если при его описании не дать ему
    аргументы

--- Задание №2 - Приложение по плану/src/Validator.java
+++ Задание №2 - Приложение по плану/src/Validator.java
@@ -10,29 +10,27 @@ public class Validator {
     CommandLineParser parser = new DefaultParser();
     Options options = new Options();
 
-    public UserInput SuperGetUserInput(UserInput ui, String[] args){
-        options.addOption("l","login", true, "Login");
-        options.addOption("pass","pass", true, "Password");
-        options.addOption("res","resource", true, "Resource");
-        options.addOption("r","role", true, "Role");
-        options.addOption("ds","ds", true, "Date Start");
-        options.addOption("de","de", true, "Date Finish");
-        options.addOption("vol","volume", true, "Volume");
-        options.addOption("h","help", false, "Help");
-        try{
-            CommandLine line = parser.parse(options,args);
-                ui.setLogin(line.getOptionValue("login"));
-                ui.setPass(line.getOptionValue("password"));
-                ui.setRes(line.getOptionValue("resource"));
-                ui.setRole(line.getOptionValue("role"));
-                ui.setDs(line.getOptionValue("ds"));
-                ui.setDe(line.getOptionValue("de"));
-                ui.setVol(line.getOptionValue("volume"));
-        }
-        catch (ParseException exp)
-        {
+    public UserInput SuperGetUserInput(UserInput ui, String[] args) {
+        options.addOption("l", "login", true, "Login");
+        options.addOption("pass", "pass", true, "Password");
+        options.addOption("res", "resource", true, "Resource");
+        options.addOption("r", "role", true, "Role");
+        options.addOption("ds", "ds", true, "Date Start");
+        options.addOption("de", "de", true, "Date Finish");
+        options.addOption("vol", "volume", true, "Volume");
+        options.addOption("h", "help", true, "Help");
+        try {
+            CommandLine line = parser.parse(options, args);
+            ui.setLogin(line.getOptionValue("login"));
+            ui.setPass(line.getOptionValue("pass"));
+            ui.setRes(line.getOptionValue("resource"));
+            ui.setRole(line.getOptionValue("role"));
+            ui.setDs(line.getOptionValue("ds"));
+            ui.setDe(line.getOptionValue("de"));
+            ui.setVol(line.getOptionValue("volume"));
+        } catch (ParseException exp) {
             HelpFormatter hf = new HelpFormatter();
-            hf.printHelp("help", options);
+            hf.printHelp("Help", options);
             System.exit(0);
         }
         return ui;
