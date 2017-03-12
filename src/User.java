051c5d6d6bf8120b740c9054df8364cb2737e792
OrdynarySkalen <voron-37@mail.ru>
2017-03-12 19:15:59 +0700

    Отформатировал класс, исправил хеширование

--- src/User.java
+++ src/User.java
@@ -1,11 +1,7 @@
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 
-/**
- * Created by Artem 2 on 08.03.2017.
- */
 public class User {
-    RandomStringUtils randStr = new RandomStringUtils();
     private String login;
     private String hashPass;
     private int id;
@@ -21,35 +17,35 @@ public class User {
         this.setHashPass(pass);
     }
 
-    public String getLogin() {
+    String getLogin() {
         return login;
     }
 
-    public void setLogin(String login) {
+    private void setLogin(String login) {
         this.login = login;
     }
 
-    public String getSalt() {
+    String getSalt() {
         return salt;
     }
 
-    public void setSalt() {
-        this.salt = randStr.randomAscii(5);
+    private void setSalt() {
+        this.salt = RandomStringUtils.randomAscii(5);
     }
 
-    public int getId() {
+    int getId() {
         return id;
     }
 
-    public void setId(int id) {
+    private void setId(int id) {
         this.id = id;
     }
 
-    public String getHashPass() {
+    String getHashPass() {
         return hashPass;
     }
 
-    public void setHashPass(String pass) {
-        this.hashPass = DigestUtils.md5Hex(pass+this.getSalt());
+    private void setHashPass(String pass) {
+        this.hashPass = DigestUtils.md5Hex(DigestUtils.md2Hex(pass) + this.getSalt());
     }
 }
