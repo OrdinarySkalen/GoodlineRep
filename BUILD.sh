7396e4a623066083346cc33fb4843214d17829a9
OrdynarySkalen <voron-37@mail.ru>
2017-03-16 01:27:33 +0700

    закинул успешный тестовый Build.sh в репозиторий
    Будет скорректирован в будущем

---
+++ BUILD.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -cp "..\commons-cli-1.3.1-bin\commons-cli-1.3.1\commons-cli-1.3.1.jar;..\commons-codec-1.10\commons-codec-1.10.jar;..\commons-lang3-3.5\commons-lang3-3.5.jar;." Application.java
