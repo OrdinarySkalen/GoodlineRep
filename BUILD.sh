9b01b2cf72e6be1a9d34fc38ea302d01ea037a15
OrdynarySkalen <voron-37@mail.ru>
2017-03-18 15:52:31 +0700

    ?

--- BUILD.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir bin #
-javac -cp "./libs/*" -d "./bin" src/*.java #
-echo "Classes create" 
-jar cvmf manifest.mf Application.jar -C ./bin / #
-echo "Jar good"
-rm -rf bin #
--- src/Roles.java
+++
@@ -1,3 +0,0 @@
-public enum Roles {
-    READ, WRITE, EXECUTE
-}
