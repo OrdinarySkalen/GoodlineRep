0d20d9c74b90e49b0bd2f5b885e1882c6256ca05
OrdynarySkalen <voron-37@mail.ru>
2017-03-18 15:02:00 +0700

    Проверяем работу тревиса

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir out2 #
-javac -cp ".\libs\*" -d "./out2" src/*.java #
+javac -cp ".\libs\commons-cli-1.3.1.jar:.\libs\commons-codec-1.10.jar:.\libs\commons-lang3-3.5.jar:." -d "./out2" src/*.java #
 echo "Classes create" 
 jar cvmf manifest.mf Application.jar -C ./out2 / #
 echo "Jar good"
