b1b7fa66378d4b4a607c885128ab5968361e8227
OrdynarySkalen <voron-37@mail.ru>
2017-04-15 21:43:06 +0700

    Полностью перенес работу с БД в DAO

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,4 @@
 import org.flywaydb.core.Flyway;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.sql.*;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -26,7 +24,6 @@ public class Application {
         UserInput userInput = new UserInput();
         AAAService service = new AAAService();
         Connection dbConnection = connector.getConnection(URL, USER, PASSWORD);
-        Statement statement;
 
         validator.getUserInput(userInput, args);
         User reqUser;
@@ -34,15 +31,17 @@ public class Application {
 
 
         try {
-            statement = dbConnection.createStatement();
+            Statement statement = dbConnection.createStatement();
             reqUser = connector.getUserFromDataBase(userInput, statement);
 
+            //Проверить существоание пользователя
             if (reqUser == null) {
                 logger.error(String.format("User %s doesn't exist.(Exit-code 1)",
                         userInput.getLogin()));
                 System.exit(1);
             }
-            //проверить пароль
+
+            //Проверить пароль
             if (!service.checkPasswordByUser(userInput.getPassword(), reqUser)) {
                 logger.error(String.format("Password %s is incorrect for the user %s.(Exit-code - 2)",
                         userInput.getPassword(), userInput.getLogin()));
@@ -59,13 +58,13 @@ public class Application {
                     System.exit(3);
                 }
 
-                //вылавливаем неизвестные ресурсы
+                //Вылавить неизвестные ресурсы
                 if (reqRes == null) {
                     logger.error(String.format("Resource %s with role %s isn't found.(Exit-code - 4)", userInput.getResource(), userInput.getRole()));
                     System.exit(4);
                 }
 
-                //проверка доступа
+                //Проверить доступ
                 if (reqUser.getId() != reqRes.getUserId()) {
                     logger.error(String.format("User %s don't have access to the resource %s.(Exit-code - 4)",
                             userInput.getLogin(), userInput.getResource()));
@@ -80,16 +79,11 @@ public class Application {
                     service.isDateValid(userInput.getDateEnd(), userInput.getDateStart());
                     service.isVolumeValid(userInput.getVolume());
 
-                    LocalDate dateE = service.tryGetDate(userInput.getDateEnd());
-                    LocalDate dateS = service.tryGetDate(userInput.getDateStart());
-                    int volume = service.tryGetVolume(userInput.getVolume());
-                    statement.executeUpdate(String.format("INSERT INTO ACCOUNTING VALUES('%s', PARSEDATETIME('%s','yyyy-MM-dd'), PARSEDATETIME('%s','yyyy-MM-dd'),'%s')",
-                            reqRes.getId(), userInput.getDateStart(), userInput.getDateEnd(), userInput.getVolume()));
-                    //result = statement.executeQuery("SELECT * FROM ACCOUNTING");
-                    /*while (result.next())
-                    {
-                        System.out.println(result.getDate("DATE_START")+" "+result.getDate("DATE_END"));
-                    }*/
+                    service.tryGetDate(userInput.getDateEnd());
+                    service.tryGetDate(userInput.getDateStart());
+                    service.tryGetVolume(userInput.getVolume());
+                    //добавляем запись о использовании ресурса в БД
+                    connector.insertRecordIntoDataBase(userInput,reqRes,statement);
                     logger.debug("Accounting: success");
                 }
             }
--- src/DAO/Connector.java
+++ src/DAO/Connector.java
@@ -1,11 +1,12 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import java.sql.*;
 
-public class Connector {
+class Connector {
     private static final Logger logger = LogManager.getLogger(Connector.class);
 
-    public Connection getConnection(String url, String user, String password) {
+    Connection getConnection(String url, String user, String password) {
         try {
             Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
@@ -19,19 +20,17 @@ public class Connector {
         return null;
     }
 
-    public static void closeConnection(Connection dbConnection)
-    {
+    static void closeConnection(Connection dbConnection) {
         try {
             dbConnection.close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.debug(e.getMessage());
         }
     }
 
-    public User getUserFromDataBase(UserInput userInput, Statement statement)
-    {
+    User getUserFromDataBase(UserInput userInput, Statement statement) {
         User user = null;
-        ResultSet result = null;
+        ResultSet result;
         try {
             result = statement.executeQuery(String.format("SELECT * FROM USER WHERE (USER.NAME LIKE '%s')", userInput.getLogin()));
             while (result.next()) {
@@ -40,25 +39,32 @@ public class Connector {
             }
             return user;
         } catch (SQLException e) {
-            e.printStackTrace();
-
+            logger.debug(e.getMessage());
         }
         return user;
     }
 
-    public Resource getResourceFromDataBase(UserInput userInput, Statement statement)
-    {
+    Resource getResourceFromDataBase(UserInput userInput, Statement statement) {
         Resource resource = null;
         ResultSet result;
         try {
-        result = statement.executeQuery(String.format("SELECT * FROM RESOURCE WHERE ((PATH='%s') or (PATH LIKE '%s')) AND (ROLE='%s')",
-                userInput.getResource(), userInput.getResource() + ".%", userInput.getRole()));
+            result = statement.executeQuery(String.format("SELECT * FROM RESOURCE WHERE ((PATH='%s') or (PATH LIKE '%s')) AND (ROLE='%s')",
+                    userInput.getResource(), userInput.getResource() + ".%", userInput.getRole()));
             while (result.next()) {
                 resource = new Resource(result.getString("PATH"), result.getInt("USER_ID"), Roles.valueOf(result.getString("ROLE")), result.getInt("ID"));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.debug(e.getMessage());
         }
         return resource;
+    }
+
+    void insertRecordIntoDataBase(UserInput userInput, Resource resource, Statement statement) {
+        try {
+            statement.executeUpdate(String.format("INSERT INTO ACCOUNTING VALUES('%s', PARSEDATETIME('%s','yyyy-MM-dd'), PARSEDATETIME('%s','yyyy-MM-dd'),'%s')",
+                    resource.getId(), userInput.getDateStart(), userInput.getDateEnd(), userInput.getVolume()));
+        } catch (SQLException e) {
+            logger.debug(e.getMessage());
+        }
     }
 }
