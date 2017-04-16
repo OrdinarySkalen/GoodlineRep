3f6c034059f930dbadd71737890205620baed09f
OrdynarySkalen <voron-37@mail.ru>
2017-04-17 00:12:41 +0700

    Продолжаем мучать Build.sh

--- BUILD.sh
+++ BUILD.sh
@@ -2,6 +2,6 @@
 mkdir bin #
 javac -cp "./libs/*" -d "./bin/" src/*.java src/DAO/Connector.java #
 echo "Classes create" 
-jar cvmf manifest.mf Application.jar -C ./bin / ./libs/ ./res/db/migration /db/migration ./res/log4j2.xml /res/ #
+jar cvmf manifest.mf Application.jar -C ./bin / ./libs/ ./res/db/migration /db/migration ./res/*.xml /res/ #
 echo "Jar good"
 rm -rf bin #
