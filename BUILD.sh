e5ad8454aa5a200435c9d767c1275a888afcc60f
OrdynarySkalen <voron-37@mail.ru>
2017-04-16 20:06:13 +0700

    Мучаю билд, безуспешно

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 mkdir bin #
+mkdir bin/pkg #
+# javac -cp "./src/DAO/*" -s "./bin/pkg/" src/DAO/*.java # fail 
+# javac -cp "./libs/*" -d "./bin" "src/DAO/*" -s "./bin/pkg/" src/*.java # fail
 javac -cp "./libs/*" -d "./bin" src/*.java #
+jar cvmf manifest.mf Application.jar -C ./bin / #
+javac -cp "./libs/*","./Application.jar" -d "./bin/pkg/" src/DAO/*.java  # fail
+# javac -cp "./libs/*" -d "./bin" src/*.java src/DAO/*.java # закидывает коннектор в корень
+# javac -cp "./libs/*" -d "./bin" -s "src/DAO" src/*.java  
 echo "Classes create" 
 jar cvmf manifest.mf Application.jar -C ./bin / #
 echo "Jar good"
