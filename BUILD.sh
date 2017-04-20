c8acb627d0634e158a027198dc0eb287f8513784
OrdynarySkalen <voron-37@mail.ru>
2017-04-20 23:37:20 +0700

    поправил билд

--- BUILD.sh
+++ BUILD.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir bin #
+javac -cp "./libs/*" -d "./bin/" src/praclab1/*.java src/praclab1/domain/*.java src/praclab1/dao/*.java #
 cp -R -v ./res/* ./bin #
 echo "Classes create" 
 jar cvmf manifest.mf praclab1.Application.jar -C ./bin / ./libs/ #
