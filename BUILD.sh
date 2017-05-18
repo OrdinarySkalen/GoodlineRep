867d7d22e01378873a29c0de748420052ec0e8ff
OrdynarySkalen <voron-37@mail.ru>
2017-05-18 17:11:22 +0700

    - лишнее

--- BUILD.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir bin #
-javac -cp "./libs/*" -d "./bin/" src/praclab1/*.java src/praclab1/domain/*.java src/praclab1/dao/*.java #
-cp -R -v ./res/* ./bin #
-echo "Classes create" 
-jar cvmf manifest.mf praclab1.Application.jar -C ./bin / ./libs/ #
-echo "Jar good"
-rm -rf bin #
--- RUN.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar PracLab1-1.0-SNAPSHOT.jar $1
